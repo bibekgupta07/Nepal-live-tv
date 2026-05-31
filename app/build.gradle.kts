13839d2cb574056d1bbb84f9bdcf65fac1298ae5
bibekgupta07 <phenomenalbibek@gmail.com>
2026-05-31 19:15:13 +0545

    chore(release): bump to 3.0.0 (versionCode 3) for v3 serverless release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.app.nepallivetv"
         minSdk = 28
         targetSdk = 36
-        versionCode = 2
-        versionName = "2.0.0"
+        versionCode = 3
+        versionName = "3.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
